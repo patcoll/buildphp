d87c928d5e5954c3adb07442cb233fee5b5f2bb8
Pat Collins <pat@193143-qb0-1.dfw1.staging.slicehost.com>
2010-02-13 16:16:38 +0000

    update mysql d/l location

--- lib/buildphp/packages/mysql.rb
+++ lib/buildphp/packages/mysql.rb
@@ -3,12 +3,12 @@ module Buildphp
     class Mysql < Buildphp::Package
       def initialize
         super
-        @version = '5.1.36'
+        @version = '5.1.43'
         @versions = {
           '5.1.36' => { :md5 => '18e694c4ecbe851fe8e21e1668116c46' },
+          '5.1.43' => { :md5 => '451fd3e8c55eecdf4c3ed109dce62f01' },
         }
         @prefix = "#{@prefix}/mysql"
-        # @prefix = "/Applications/MAMP/Library"
       end
 
       def package_depends_on
@@ -27,7 +27,7 @@ module Buildphp
       end
 
       def package_location
-        'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/%s' % package_name
+        'ftp://mirror.anl.gov/pub/mysql/Downloads/MySQL-5.1/%s' % package_name
       end
 
       def php_config_flags
